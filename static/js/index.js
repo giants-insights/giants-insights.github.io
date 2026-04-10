80b8f83170920c437096d1ecefaa34f585ce4211
Anikait Singh <singh.anikait@gmail.com>
2026-04-10 14:41:48 -0700

    dark mode

--- static/js/index.js
+++ static/js/index.js
@@ -18,4 +18,37 @@ $(document).ready(function() {
 	
     bulmaSlider.attach();
 
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    const darkModeIcon = document.getElementById('darkModeIcon');
+    const body = document.body;
+
+    // Check for saved user preference, if any, on load of the website
+    const darkMode = localStorage.getItem('darkMode');
+    
+    // If user has chosen dark mode in the past, apply it
+    if (darkMode === 'enabled') {
+        body.classList.add('dark-mode');
+        if (darkModeIcon) {
+            darkModeIcon.classList.remove('fa-moon');
+            darkModeIcon.classList.add('fa-sun');
+        }
+    }
+
+    if (darkModeToggle) {
+        darkModeToggle.addEventListener('click', () => {
+            body.classList.toggle('dark-mode');
+            
+            // Check if dark mode is active and save preference
+            if (body.classList.contains('dark-mode')) {
+                localStorage.setItem('darkMode', 'enabled');
+                darkModeIcon.classList.remove('fa-moon');
+                darkModeIcon.classList.add('fa-sun');
+            } else {
+                localStorage.setItem('darkMode', null);
+                darkModeIcon.classList.remove('fa-sun');
+                darkModeIcon.classList.add('fa-moon');
+            }
+        });
+    }
+
 })
